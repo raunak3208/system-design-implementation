c4832eab526718f93f23e4e5c3e6be5aca0976d4
Raunak Kumar Jha <raunak3208@users.noreply.github.com>
2026-06-16 21:23:25 +0530

    User data added

---
+++ api/authentication-authorization/data/users.js
@@ -0,0 +1,18 @@
+const bcrypt = require("bcryptjs");
+
+const users = [
+  {
+    id: 1,
+    username: "admin",
+    password: bcrypt.hashSync("admin123", 10),
+    role: "admin"
+  },
+  {
+    id: 2,
+    username: "user",
+    password: bcrypt.hashSync("user123", 10),
+    role: "user"
+  }
+];
+
+module.exports = users;
